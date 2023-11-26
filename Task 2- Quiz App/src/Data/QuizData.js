c199addcdb6ea942cb1cf232d592e9acb6508e45
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-11-26 15:57:25 +0530

    task 2

---
+++ Task 2- Quiz App/src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "What is your name?",
+        options:[ "Me", "Chintu", "Ritesh", "Siddharth"],
+        answer: 1
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", " 1998"],
+        answer:2
+    },
+];
